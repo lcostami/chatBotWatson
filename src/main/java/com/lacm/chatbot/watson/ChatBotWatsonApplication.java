5f693975f6103adc9433b726f26660326e783630
LUIS ANTONIO COSTA MIGUEL <0015594@insags.com>
2020-03-10 10:47:27 +0100

    Primer Commit

---
+++ src/main/java/com/lacm/chatbot/watson/ChatBotWatsonApplication.java
@@ -0,0 +1,13 @@
+package com.lacm.chatbot.watson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBotWatsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBotWatsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lacm/chatbot/watson/ChatBotWatsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lacm.chatbot.watson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBotWatsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
